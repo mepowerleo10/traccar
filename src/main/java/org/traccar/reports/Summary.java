d924e7ac8c66857e961aedae9634f76567f4330b
Mussa Mipawa <mepowerleo10@gmail.com>
2023-01-21 13:51:36 +0300

    fix: remove unwanted imports

--- src/main/java/org/traccar/reports/Summary.java
+++ src/main/java/org/traccar/reports/Summary.java
@@ -20,7 +20,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
